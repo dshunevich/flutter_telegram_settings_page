c0aa39773a838fbb9ae5e869fb85e5b1f123f411
dshunevich <dmitryshunevich@gmail.com>
2021-09-15 16:59:19 +0300

    first commit

---
+++ lib/widgets/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/widgets/user_profile.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: UserProfile(), //Text('asssdasda'),
+    );
+  }
+}
---
+++ lib/widgets/user_profile.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class UserProfile extends StatelessWidget {
+  const UserProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Настройки'),
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            _AvatarWidget(),
+            SizedBox(height: 20),
+            _UserNameWidget(),
+            SizedBox(height: 10),
+            _UserPhoneWidget(),
+            SizedBox(height: 10),
+            _UserNickNameWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _UserNickNameWidget extends StatelessWidget {
+  const _UserNickNameWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('@dshunevich');
+  }
+}
+
+class _UserPhoneWidget extends StatelessWidget {
+  const _UserPhoneWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '+7(999)999 99 99',
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 15,
+      ),
+    );
+  }
+}
+
+class _UserNameWidget extends StatelessWidget {
+  const _UserNameWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Dmitry Shunevich',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+}
+
+class _AvatarWidget extends StatelessWidget {
+  const _AvatarWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      child: Placeholder(),
+    );
+  }
+}
